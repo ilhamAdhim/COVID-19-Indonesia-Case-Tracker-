6b347ba3f444b8425b64f37eebb57f2c539bda05
ilhamadhim <ilhamm179@gmail.com>
2020-06-08 23:02:18 +0700

    Connect dependencies

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import main from "../src/view/main";
-// import "./script/component/AppBar";
-// import "./script/component/SearchBar";
-// import './script/component/ClubList';
-
 import "../styles/style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import "./component/NavBar";
+import "./component/Region";
+import "./component/RegionList";
+
 
 
 document.addEventListener("DOMContentLoaded", main);
