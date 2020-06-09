09881794eac37aa0b7299b09e43d522c6c4e5be1
ilhamadhim <ilhamm179@gmail.com>
2020-06-09 20:02:36 +0700

    Differ Province and region fetch data

--- src/view/main.js
+++ src/view/main.js
@@ -6,12 +6,7 @@ const main = () => {
     const defaultNations = ['China', 'US', 'Indonesia'];
     const defaultProvinces = ['DKI Jakarta', 'Jawa Timur', 'Jawa Tengah', 'Jawa Barat', 'Sumatera Utara'];
 
-    const container = document.querySelector('.container');
     const regionListElement = document.querySelector("region-list");
-
-    const importAllImages = (image) => {
-        return image.keys().map(image);
-    }
 
     const dataByProvince = async (province = null) => {
         try {
@@ -23,11 +18,9 @@ const main = () => {
                     let province = _.filter(resultAPI.features, ['attributes.Provinsi', provinceName])
                     finalResult.push(province);
                 })
-                // console.log("Processed ", JSON.stringify(finalResult))
             }
-            // console.log("Raw", resultAPI)
 
-            renderResult(province != null ? finalResult : resultAPI);
+            renderProvinceData(province != null ? finalResult : resultAPI);
         } catch (message) {
             errorMessage(message);
         }
@@ -38,14 +31,12 @@ const main = () => {
             const finalResult = []
             const resultAPI = await DataSource.getDataByCountry(country);
             if (country != null) {
-                // console.log(resultAPI.features[0].attributes.Country_Region)
                 defaultNations.forEach(nationName => {
                     let country = _.filter(resultAPI.features, ['attributes.Country_Region', nationName])
                     finalResult.push(country);
                 })
-                // console.log(finalResult)
             }
-            renderResult(country != null ? finalResult : resultAPI);
+            renderCountryData(country != null ? finalResult : resultAPI);
 
         } catch (message) {
             errorMessage(message);
@@ -57,14 +48,33 @@ const main = () => {
         console.log(msg);
     }
 
-    const renderResult = results => {
+    const renderCountryData = results => {
         regionListElement.regions = results;
     };
 
+    const renderProvinceData = results => {
+        const container = document.querySelector('.data-province');
+        results.forEach(province => {
+            container.innerHTML += `<tr>`;
 
-    // dataByProvince(defaultProvinces);
-    dataAllCountry(defaultNations);
+            console.log(province[0])
+            container.innerHTML += ` 
+            <td> <center> ${province[0].attributes.Provinsi} </center></td>
+            <td> <center> ${province[0].attributes.Kasus_Meni} </center></td>
+            <td> <center> ${province[0].attributes.Kasus_Semb} </center></td>
+            <td> <center> ${province[0].attributes.Kasus_Posi} </center></td>
+            `
+            container.innerHTML += `</tr>`;
 
+        })
+    }
+
+    const defaultData = async () => {
+        await dataAllCountry(defaultNations);
+        await dataByProvince(defaultProvinces);
+    }
+
+    defaultData()
 
 }
 
