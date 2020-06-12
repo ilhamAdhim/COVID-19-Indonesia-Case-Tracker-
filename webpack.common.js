6d855ae4dbdbe20a0ad3dd8d98f0c38e08d6698c
ilhamadhim <ilhamm179@gmail.com>
2020-06-12 11:57:10 +0700

    Remove unusde loader

--- webpack.common.js
+++ webpack.common.js
@@ -22,36 +22,6 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                    'file-loader',
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                                quality: 75
-                            }
-                        }
-                    },
-                ],
-            },
-            {
                 test: /\.(jpg|png)$/,
                 use: {
                     loader: 'url-loader',
