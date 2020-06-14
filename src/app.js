afc70dd681d9453a69dbc338034ce7b0d50baf4c
ilhamadhim <ilhamm179@gmail.com>
2020-06-14 13:45:16 +0700

    Added regenerator runtime for async await codes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import main from "../src/view/main";
+import 'regenerator-runtime'
+
 import "../styles/style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
@@ -7,5 +9,6 @@ import "./component/RegionItem";
 import "./component/RegionList";
 import "./component/SearchBar";
 import "./component/ProvinceItem";
+
 
 document.addEventListener("DOMContentLoaded", main);
