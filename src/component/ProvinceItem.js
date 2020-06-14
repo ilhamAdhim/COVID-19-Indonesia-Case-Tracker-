72728db3e1143a4a6dd10d7204e19b54745511b2
ilhamadhim <ilhamm179@gmail.com>
2020-06-15 01:10:31 +0700

    remove renderError

--- src/component/ProvinceItem.js
+++ src/component/ProvinceItem.js
@@ -4,20 +4,13 @@ class ProvinceItem extends HTMLElement {
     }
 
     set province(province) {
+        this._tbody = document.querySelector('tbody');
         // Get only some keys from JSON Response and assign it to private variable
         this._province = province[0].attributes;
         this.render();
     }
-
-    renderError() {
-        this.innerHTML = `Provinsi tidak ditemukan`;
-    }
-
-
     render() {
-        const tbody = document.querySelector('tbody');
-
-        tbody.innerHTML += ` 
+        this._tbody.innerHTML += ` 
             <tr class="province-item">
                 <td style="font-size:1em" class="text-center"> <b> ${this._province.Provinsi} </b> </td>
                 <td style="font-size:1em" class="text-center"> <b> ${this._province.Kasus_Meni} </b> </td>
