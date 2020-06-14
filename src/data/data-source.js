f977dfe7593f793ba0b655bac8dddbb178e12d4d
ilhamadhim <ilhamm179@gmail.com>
2020-06-15 01:10:45 +0700

    Delete Async keyword on promises

--- src/data/data-source.js
+++ src/data/data-source.js
@@ -1,5 +1,5 @@
 class DataSource {
-    static async getDataByCountry(country = null) {
+    static getDataByCountry(country = null) {
 
         // Confirmed, recovered, death data from certain country
         return fetch(`https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2/query?where=1%3D1&outFields=*&outSR=4326&f=json`)
@@ -8,12 +8,12 @@ class DataSource {
             .catch(err => Promise.reject(`${country} is not found`));
     }
 
-    static async getDataByProvince() {
+    static getDataByProvince() {
         // Province, Deaths,Recovered, Positives data from Indonesia
         return fetch(`https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json`)
             .then(response => response.json())
             .then(responseJSON => Promise.resolve(responseJSON))
-            .catch(err => Promise.reject(`Data is not found`));
+            .catch(err => Promise.reject(err));
     }
 
 }
